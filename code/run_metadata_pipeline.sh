0a5bcb74d79a796cdbb9f90eb04305a29ca463bf
Jaren Haber <jhaber@berkeley.edu>
2021-03-09 10:40:20 -0600

    update folders and output format: h5

--- code/run_metadata_pipeline.sh
+++ code/run_metadata_pipeline.sh
@@ -1,8 +1,8 @@
 for i in {1..44}
 do
-    nohup python3 ParseMetaFilesUpdated.py /vol_b/data/jstor_data $i 44 ./metadata_results/ &
+    nohup python3 ParseMetaFilesUpdated.py /vol_b/data/jstor_data $i 44 /vol_b/data/models_storage/metadata/ &
 done
 
 wait
 
-python3 merge_metadata_result.py ./metadata_results/h5/ 44 ../MetaData/journal\ titles\ \&\ subjects\ 10-15\ edited.csv ./output/metadata_combined.h5
+python3 merge_metadata_result.py /vol_b/data/models_storage/metadata/ 44 ../metadata/journal\ titles\ \&\ subjects\ 10-15\ edited.csv /vol_b/data/models_storage/metadata/metadata_combined.h5
